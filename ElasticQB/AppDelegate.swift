efeee09e4fcf5f46334d8ea768d5e9be1b9bda9a
Matt Mondok <matt.mondok@gmail.com>
2015-05-12 22:31:01 -0400

    Initial Commit

---
+++ ElasticQB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ElasticQB
+//
+//  Created by Matthew Mondok on 5/12/15.
+//  Copyright (c) 2015 Matthew Mondok. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ElasticQB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ElasticQB
+//
+//  Created by Matthew Mondok on 5/12/15.
+//  Copyright (c) 2015 Matthew Mondok. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
