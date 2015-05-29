a426b4d127cdcf3cf55b65796be28ea752818f9d
mondok <mmondok@salesforce.com>
2015-05-28 22:21:29 -0400

    Added in call to mappings and Alamofire

--- ElasticQB/ElasticQBViewController.swift
+++ ElasticQB/ElasticQBViewController.swift
@@ -8,11 +8,15 @@
 
 import Cocoa
 
+
 class ElasticQBViewController: NSViewController {
+
+    @IBOutlet var _resultText: NSTextView!
 
     @IBOutlet weak var _url: NSTextField!
     override func viewDidLoad() {
         self.title = "ElasticQB"
+        _url.stringValue = "http://localhost:9200/_mappings"
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -25,5 +29,23 @@ class ElasticQBViewController: NSViewController {
     }
 
 
+    @IBAction func updateMappingsClicked(sender: NSButton) {
+        request(.GET,  _url.stringValue)
+            .responseJSON { (_, _, JSON, _) in
+                self._resultText.string = self.JSONStringify(JSON!, prettyPrinted: true)
+        }
+    }
+    
+    func JSONStringify(value: AnyObject, prettyPrinted: Bool = false) -> String {
+        var options = prettyPrinted ? NSJSONWritingOptions.PrettyPrinted : nil
+        if NSJSONSerialization.isValidJSONObject(value) {
+            if let data = NSJSONSerialization.dataWithJSONObject(value, options: options, error: nil) {
+                if let string = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                    return string as String
+                }
+            }
+        }
+        return ""
+    }
 }
 
